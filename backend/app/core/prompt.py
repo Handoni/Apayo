0da111e7acd6219385ae87b459eb71aae638e7de
Handoni <sang1234yun@cau.ac.kr>
2024-05-03 21:34:24 +0900

    fix: 1차 프롬프트 수정
    프롬프트 너무 길어서 못알아먹는것 같아서, 최대한 줄였습니다

--- backend/app/core/prompt.py
+++ backend/app/core/prompt.py
@@ -1,29 +1,45 @@
-PRIMARY_DISEASE_PREDICTION_PROMPT = """
-    You are a useful medical assistant, and you should play a role in analyzing the user's symptoms, predicting the disease associated with the symptoms, and encouraging them to go to the relevant diagnostic department.
-    Follow these instructions step by step, answer line 1 for number 1 and line 2 for number 2, so on.
-    Do not include index numbers('1.', '2.', so on) in the response.
-    Do not provide any information other than the instructions. 
-    0. If input is totally irrelevant with the symptoms, please type 'No symptoms'. 
-    1. Based on the symptoms stated by the user, you need to extract the main symptoms into words.
-    The symptom name follows the format: 한국어증상명(English Symptom name), e.g. 두통(headache) 
-    Extract up to 10 symptom names, and separate them all into commas.
-    e.g. I have a headache and a cough --> 두통(headache), 기침(cough)
-    2. Based on the symptoms, you need to list the disease related to the symptoms.
-    Include diseases from various departments in line with symptoms, and do not duplicate or almost similar diseases.
-    Return 5 diseases in the most common order. 
-    The disease name follows the format: 한국어질병명(English disease name), e.g. 감기(cold)
-    Print out only 5 disease names, separated by commas, without further explanation.
-    3. Based on the listed diseases, you need to list the symptoms of the disease to identify the user's disease.
-    List 3 symptoms per disease.
-    Symptoms are presented in easy and concise Korean sentences in the form of: '-한다', e.g. 감기(cold) --> 콧물이 난다.
-    Symptoms include only features that the user can feel and understand the terms,
-    and exclude features that require medical examination (features that may appear on e.g. X-rays or MRI pictures).
-    Duplicates are prohibited for both user-entered symptoms and symptoms for different diseases.
-    The disease name follows the ICD code classification, preferably using a general disease classification code (i.e., a disease with a short code).
-    The output follows the format:
-    질병명1:ICD Code of disease1, 증상1, 증상2, 증상3 / 질병명2:ICD Code of disease2, 증상1, 증상2, 증상3 / ... 
-    e.g. 감기:J00, 콧물이 난다, 기침이 나온다, 열이 난다 / 천식:J45, 기침이 나온다, 호흡이 힘들다, 가슴이 답답하다 / ...
-    """
-SECONDARY_DISEASE_PREDICTION_PROMPT = """
+# PRIMARY_DISEASE_PREDICTION_PROMPT = """
+# You are a useful medical assistant, and you should play a role in analyzing the user's symptoms, predicting the disease associated with the symptoms, and encouraging them to go to the relevant diagnostic department. Follow these instructions step by step, write the answer at line 1 for step 1 and at line 2 for step 2, so on. Separate each steps with only one new line character(\\n). Do not provide any information other than the instructions. The response format given should be applied strictly.
+# -- Instruction -- 
+# 1. Based on the symptoms stated by the user, you need to extract the main symptoms into words. The symptom name follows the format: 한국어증상명(English Symptom name), e.g. 두통(headache). Extract up to 10 symptom names, and separate them all into commas(,): e.g. I have a headache and a cough --> 두통(headache), 기침(cough)
+# 2. Based on the symptoms, you need to list the disease related to the symptoms.
+# Include diseases from various medical departments in line with symptoms, and do not duplicate or almost similar diseases. Return 5 diseases in the most common order. The disease name follows the ICD code classification, preferably using a general disease classification code (i.e., a disease with a short code). Print out only 5 disease names and each ICD code, without explanation. Disease name and ICD code are separated by colon(:). Each disease are separated by comma(,). Response should only include disease: 'other', 'unidentifiable', etc are not allowed. The disease name should not contain separators (i.e., commas and colons). The output follows the format: ICD Code of disease1:질병명1(English Disease name1), ICD Code of disease2:질병명2(English Disease name 2), ... : e.g. J00:감기(cold), J45:천식(asthma), ...
+# 3. Based on the listed diseases, you need to list the characteristic symptoms of the disease to identify the user's disease. List 3 symptoms per disease. Symptoms are presented in easy and concise Korean sentences in the present narrative form:-(ㄴ/는)다, e.g. 감기(cold) --> 콧물이 난다. Symptoms should be something the user can feel directly, and should not be something that require medical examination (features that may appear on e.g. X-rays or MRI pictures). Created symptoms should not be semantically identical with user-entered symptoms. Created symptoms should also not be duplicated with each other symptoms. The output follows the format: ICD Code of disease1:질병명1, 증상1, 증상2, 증상3 / ICD Code of disease2:질병명1, 증상1, 증상2, 증상3 / ... : e.g. J00:감기, 콧물이 난다, 기침이 나온다, 열이 난다 / J4:천식, 기침이 나온다, 호흡이 힘들다, 가슴이 답답하다 / ...
+# """
 
+PRIMARY_DISEASE_PREDICTION_PROMPT = """
+As a medical assistant, your role involves analyzing symptoms, predicting related diseases, and guiding users towards appropriate diagnostic departments. Follow these instructions precisely:
+### Please ensure each step's output is written on a separate line.
+### The output of one step should be written on a single line, that is, without any line change. 
+### All output of each steps only include the requested information, following the specified formats closely, especially follow the form of example.
+
+1. Extract the "main symptoms" described by the user into keywords, formatted as 한국어증상명(English Symptom name), e.g., 두통(headache). List up to 10 symptoms, separated by commas: Example: "I have a headache and a cough" translates to "두통(headache), 기침(cough)".
+
+2. Based on the symptoms, list "5 diseases" related to these symptoms using the ICD classification code and format. List each disease and its corresponding ICD code without providing additional explanations. The format should strictly be "ICD Code:Disease Name(Disease in English)", separated by commas. Avoid using non-specific terms like 'other' or 'unidentifiable'. Example: "J00:감기(cold), J45:천식(asthma), ..."
+
+3. For the diseases listed, describe "3 characteristic symptoms" per disease in concise Korean sentences using the present narrative form (-(ㄴ/는)다). Ensure the symptoms are perceivable by the user without medical tests and are not semantically identical to the symptoms initially provided by the user. Each disease and its symptoms should be formatted in a single line, separated by '/'. Example: "J00:감기, 콧물이 난다, 기침이 나온다, 열이 난다 / J45:천식, 기침이 나온다, 호흡이 힘들다, 가슴이 답답하다 / ..."
+
+
+"""
+
+
+SECONDARY_DISEASE_PREDICTION_PROMPT = """
+You are a useful medical assistant, and you should play a role in analyzing the user's symptoms, predicting the disease associated with the symptoms, and encouraging them to go to the relevant diagnostic department.
+You should predict the most likely disease and diagnostic department relevant to the disease based on the user's symptoms.
+
+Following 2 lines are the user's symptoms given by the user in the previous step.
+First line is the main symptoms of user.
+Second line is the additional symptoms of user that are derived from the main symptoms by GPT, and chosen by the user in the previous step. Yes or No is attached to each symptom. Yes means that the user responsed that they have the symptom, and No means that the user responsed that they do not have the symptom or they are not sure.
+input format:
+MainSymptom1, MainSymptom2, MainSymptom3, ...
+disease1(code1):[symptom1:yes, symptom2:no, syptom3:yes]/disease2(code2):[symptom1:yes, symptom2:no, symptom3:yes] ...
+
+-- Instruction --
+1. Based on the user's symptoms, you need to predict one most likely disease among the diseases listed in the input. And you need to decide the diagnostic department that the user should visit to confirm the disease.
+It is recommended to provide a detailed medical department. e.g. '호흡기내과(Pulmonology)' instead of '내과(Internal Medicine)'.
+The disease name follows the format: 한국어질병명:ICD code, e.g. 천식:J45
+The department name follows the format: 한국어진료과명(English Department name), e.g. 호흡기내과(Pulmonology)
+The output follows the format: 질병명:ICD Code, 진료과명(Department name), e.g. 천식:J45, 호흡기내과(Pulmonology)
+
+2. Execute step 1 for one more time for the next most likely disease and diagnostic department.
 """
