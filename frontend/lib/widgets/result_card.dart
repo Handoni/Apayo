7e31622544545e62b9059dd09836c14b42f4b60e
thdudgus <gusthdud527@cau.ac.kr>
2024-05-03 22:41:32 +0900

    feat : 증상 질문 버튼 추가
    결과 버튼 UI 추가. 연동 필요.

--- frontend/lib/widgets/result_card.dart
+++ frontend/lib/widgets/result_card.dart
@@ -2,84 +2,81 @@ import 'package:flutter/material.dart';
 
 class ResultCard extends StatelessWidget {
   final String disease, description, dept;
+  final int n;
 
-  final _blackColor = const Color(0xffD9D9D9);
+  final greybackground = const Color(0xffD9D9D9);
 
   const ResultCard({
     super.key,
     required this.disease,
     required this.description,
     required this.dept,
+    required this.n,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // clipBehavior는 어떤 아이템이 overflow가 되었을 때, 카드와 같은 container가 어떻게 동작하게 하는지 알려줌.
-      // container의 바깥 부분을 컨트롤
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: _blackColor,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    color: isInverted ? _blackColor : Colors.white,
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.scale(
-              // 아이콘만 크기 변경 위함 (카드 크기 변화 x)
-              scale: 2.2,
-              child: Transform.translate(
-                offset: const Offset(-5, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted
-                      ? _blackColor
-                      : const Color.fromARGB(255, 255, 255, 255),
-                  size: 88,
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Row(
+            children: [
+              Text(
+                '    진단 결과를 알려드릴게요!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
                 ),
               ),
-            )
-          ],
-        ),
+              Text(
+                '  (병원에 방문하실 때 참고해보세요)',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+              color: greybackground,
+              // 카드 모서리를 둥글게 처리
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // 결과
+                children: [
+                  Text(
+                    '예상 질병 $n 순위 : $disease\n',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  Text(
+                    '$description\n',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(
+                    '추천 진료과 : $dept',
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
