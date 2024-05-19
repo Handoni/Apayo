807bcaf82f60e720854e09562a7bd62324d7e533
Handoni <sang1234yun@naver.com>
2024-05-20 00:16:44 +0900

    refactor: 데이터 처리 로직 개선
    나중에 다시 분석 요청해도 잘 돌아가도록 수정했음

--- backend/app/utils/data_processing.py
+++ backend/app/utils/data_processing.py
@@ -8,6 +8,46 @@ from fastapi import HTTPException
 from api.schemas.secondary_disease_prediction import PredictedDisease
 
 
+def parse_symptoms(symptoms: str) -> list:
+    if "no symptoms" in symptoms.lower():
+        return None
+    return {
+        str(uuid4()): _.strip() for _ in symptoms.replace("1.", "").split("|")
+    }
+
+def parse_diseases(diseases: str) -> list:
+    raw_diseases = diseases.replace("2.", "").replace("ICD code:", "").split("|")
+
+    diseases_dict = {}
+    for i in raw_diseases:
+        code, name = i.split(":")
+        code = code.strip()
+        name = name.strip()
+        diseases_dict[code] = name
+
+    return diseases_dict
+
+def parse_questions(questions: str) -> list:
+    temp = []
+    print(re.split(r"\n+|/", questions))
+    for pair in re.split(r"\n+|/", questions):
+        temp.append(
+            [
+                _.strip()
+                for _ in pair.replace("3.", "").replace("ICD code:", "").split("|")
+            ]
+        )
+
+    question = {}
+    for i in temp:
+        code, name = i[0].split(":")
+        code = code.strip()
+        name = name.strip()
+
+        question[code] = {str(uuid4()): j for j in i[1:]}
+
+    return question
+"""
 def parse_primary_response(response: str) -> list:
     if "no symptoms" in response.lower():
         return None
@@ -48,14 +88,20 @@ def parse_primary_response(response: str) -> list:
         question[code] = {str(uuid4()): j for j in i[1:]}
 
     return symptoms, diseases, question
-
+"""
 
 def create_secondary_input(input_data: UserQuestionResponse) -> str:
     print(input_data.responses)
-    result = ""
     session = SessionManager.get_session(input_data.session_id)
+    result = ""
+
+    result += "User Symptoms:" + session.user_input + "\n"
+    
+    result += "Extracted Symptoms:"
     result += ", ".join(session.primary_symptoms.values())
     result += "\n"
+
+    result += "Predicted Diseases:"
     result += ", ".join(
         ["{}:{}".format(k, v) for k, v in session.primary_diseases.items()]
     )
@@ -70,22 +116,18 @@ def create_secondary_input(input_data: UserQuestionResponse) -> str:
             raise HTTPException(status_code=404, detail="Question not found")
         temp.append(f"{merged_questions[id]}:{response}")
 
+    result += "Additional Symptoms: "
     result += ", ".join(temp)
 
     return result
 
 
-def parse_secondary_response(response: str) -> list[PredictedDisease]:
-    response = re.split(r"\n+", response)
-    print(response)
-    if len(response) > 2 or len(response) < 1:
-        raise HTTPException(status_code=404, detail="Invalid response")
-    result = {}
-    for i in response:
-        temp = i.split("|")
-        result = PredictedDisease(
-            Disease=temp[0].strip(),
-            recommended_department=temp[1].strip(),
-            description=temp[2].strip(),
-        )
+def parse_secondary_response(response: str) -> PredictedDisease:
+    
+    temp = response.split("|")
+    result = PredictedDisease(
+        Disease=temp[0].strip(),
+        recommended_department=temp[1].strip(),
+        description=temp[2].strip(),
+    )
     return result
