435750791925651fc53ca9c201086bb306679bbc
shu030929 <shu030929@gmail.com>
2024-06-12 22:52:38 +0900

    feat: 병원 api 구현 완료

--- frontend/lib/google_map.dart
+++ frontend/lib/google_map.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart' as http; // HTTP 요청용
-import 'dart:convert'; // JSON 디코딩용
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class MapScreen extends StatefulWidget {
   @override
@@ -11,27 +11,26 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
-  LatLng? _currentPosition; // 초기 위치용, nullable한 LatLng 타입
-  List<Marker> _markers = []; // 병원 위치 마커 리스트
+  LatLng? _currentPosition;
+  List<Marker> _markers = [];
+  Map<String, dynamic>? _selectedHospital;
 
   @override
   void initState() {
     super.initState();
-    _determinePosition(); // 현재 위치함수 호출
+    _determinePosition();
   }
 
   Future<void> _determinePosition() async {
-    // 위치 불러오기 함수
     bool serviceEnabled;
     LocationPermission permission;
 
-    // 권한 요청
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       return Future.error('Location services are disabled.');
     }
 
-    permission = await Geolocator.checkPermission(); // 요청 될때까지 로딩
+    permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
@@ -44,48 +43,52 @@ class _MapScreenState extends State<MapScreen> {
     }
 
     Position position = await Geolocator.getCurrentPosition(
-        // 위치 가져오기 성공하면
         desiredAccuracy: LocationAccuracy.high);
     setState(() {
-      _currentPosition = LatLng(
-          position.latitude, position.longitude); // _currentPosition 업데이트
+      _currentPosition = LatLng(position.latitude, position.longitude);
     });
-    print("여기까진 성공~");
-    // 위치가 업데이트되면 주변 병원을 검색
     _getNearbyHospitals();
   }
 
   Future<void> _getNearbyHospitals() async {
     if (_currentPosition == null) return;
+    const String url = 'http://52.79.91.82/api/get_hospitals';
 
-    const String apiKey = 'AIzaSyD4uENvK0mfa6lIvQFngXHwGp369y-zyoA';
-    //백에서 진료과를 넘겨주면 keyword에 저장하는 로직 추가해야됨
-    final String keyword = '정형외과';
-    final String url =
-        'http://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_currentPosition!.latitude},${_currentPosition!.longitude}&radius=2000&&keyword=$keyword&key=$apiKey';
+    final Map<String, dynamic> requestBody = {
+      "xPos": _currentPosition!.longitude,
+      "yPos": _currentPosition!.latitude,
+      "department": "정형외과"
+    };
 
-    final response = await http.get(Uri.parse(url));
-    print(response.body);
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode(requestBody),
+    );
+
     if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      final List<dynamic> results = data['results'];
+      final data = json.decode(utf8.decode(response.bodyBytes));
+      final List<dynamic> items = data['items'];
 
       setState(() {
-        _markers = results.map((result) {
-          //result 리스트
+        _markers = items.map((item) {
           final LatLng position = LatLng(
-            result['geometry']['location']['lat'],
-            result['geometry']['location']['lng'],
+            double.parse(item['yPos']),
+            double.parse(item['xPos']),
           );
-
           return Marker(
-            //마커로 변환
-            markerId: MarkerId(result['place_id']),
+            markerId: MarkerId(item['yadmNm']),
             position: position,
             infoWindow: InfoWindow(
-              title: result['name'],
-              snippet: result['vicinity'],
+              title: item['yadmNm'],
             ),
+            onTap: () {
+              setState(() {
+                _selectedHospital = item;
+              });
+            },
           );
         }).toList();
       });
@@ -97,9 +100,7 @@ class _MapScreenState extends State<MapScreen> {
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
     if (_currentPosition != null) {
-      // _currentPostition 있으면,
       mapController.moveCamera(
-        // 카메라 이동
         CameraUpdate.newLatLng(_currentPosition!),
       );
     }
@@ -109,18 +110,124 @@ class _MapScreenState extends State<MapScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Google Maps'),
-      ),
-      body: _currentPosition == null
-          ? Center(child: CircularProgressIndicator())
-          : GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _currentPosition!,
-                zoom: 17.0,
+        title: const Row(
+          children: [
+            Text(
+              'APAYO TEAM 6',
+              style: TextStyle(
+                color: Color.fromARGB(255, 94, 94, 94),
+                fontSize: 10,
+                fontWeight: FontWeight.bold,
               ),
-              markers: Set<Marker>.of(_markers),
             ),
+            SizedBox(width: 10),
+            Text(
+              '         RYU SOOJUNG         LEE SANGYUN         HYUN SOYOUNG',
+              style: TextStyle(
+                color: Color.fromARGB(255, 94, 94, 94),
+                fontSize: 10,
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        toolbarHeight: 70,
+        titleSpacing: 50, //앱바 왼쪽 간격추가
+      ),
+      // ~~~~~~~~~~~~~~~~~~~~~~app Bar~~~~~~~~~~~~~~~~~~~~~~~
+
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.3,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 218, 230, 255),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "내 주변에 있는 정형외과",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          const SizedBox(
+            child: Text(
+              "표시된 전문의 수는 병원 전체의 전문의 수 입니다",
+              style: TextStyle(fontSize: 12, color: Colors.black),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.9, // 가로 크기 조정
+                height: MediaQuery.of(context).size.height * 0.75, // 세로 크기 조정
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), // 모서리 둥글게 설정
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 1,
+                      blurRadius: 7,
+                      offset: const Offset(0, 1), // 그림자 위치 설정
+                    ),
+                  ],
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20), // 모서리 둥글게 설정
+                  child: _currentPosition == null
+                      ? const Center(child: CircularProgressIndicator())
+                      : GoogleMap(
+                          onMapCreated: _onMapCreated,
+                          initialCameraPosition: CameraPosition(
+                            target: _currentPosition!,
+                            zoom: 16.0,
+                          ),
+                          markers: Set<Marker>.of(_markers),
+                          myLocationEnabled: true, // 내 위치 표시
+                          myLocationButtonEnabled: true, // 내 위치 버튼 표시
+                        ),
+                ),
+              ),
+            ),
+          ),
+          if (_selectedHospital != null)
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        _selectedHospital!['yadmNm'],
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text('주소: ${_selectedHospital!['addr']}'),
+                      Text('전화번호: ${_selectedHospital!['telno']}'),
+                      Text('전문의 수: ${_selectedHospital!['specialistCount']}'),
+                      Text('n차 병원: ${_selectedHospital!['hospitalGrade']}'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
